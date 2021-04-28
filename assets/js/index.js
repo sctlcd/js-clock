42c19e6b7833fdc8ed71b42228e504cc02b7fb22
8 <lucinda.souchet@outlook.com>
2021-04-28 19:13:09 +0200

    update style

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,7 @@
 const secondHand = document.querySelector('.second-hand');
 const minuteHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
+const secondHandhand = document.querySelector('.hand');
 
 function setDate() {
   const now = new Date();
